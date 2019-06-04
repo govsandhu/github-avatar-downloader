1e15b1f7fc9affa84d848f29c55838cb3275664b
Govind Sandhu <gc.sandhu@gmail.com>
2019-06-04 03:08:33 +0000

    Required arguments within the function

--- download_avatars.js
+++ download_avatars.js
@@ -5,45 +5,43 @@ var fs = require('fs');
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-   var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': 'token ' + tokeKey.GITHUB_TOKEN
-      }
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + tokeKey.GITHUB_TOKEN
+        }
     };
 
-   request(options, function(err, res, body) {
-    var newObj = JSON.parse(body);
-    cb(err, newObj);
-   });
-
-
-
-
-
+    request(options, function(err, res, body) {
+        if (res.statusCode === 200) {
+            var newObj = JSON.parse(body);
+            cb(err, newObj);
+        } else {
+            console.log(res.statusCode + ": Repo not found")
+        }
+    });
 }
 
 function downloadImageByURL(url, filePath) {
-      request.get(url)
-      .pipe(fs.createWriteStream(filePath))
+    request.get(url)
+        .pipe(fs.createWriteStream(filePath))
 }
 
-// Below is just to call the function while building.
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  var avatarURL = "";
-  var login = "";
+var owner = process.argv[2]
+var repo = process.argv[3]
+if (!repo) {
+    console.log("Errors: Missing parameter");
+} else {
+    getRepoContributors(owner, repo, function(err, result) {
 
-  for(i of result) {
-    var avatarUrl = i.avatar_url
-    var filePath = "avatars/" + i.login + ".jpg"
-    downloadImageByURL(avatarUrl, filePath)
+        var avatarURL = "";
+        var login = "";
+        for (i of result) {
+            var avatarUrl = i.avatar_url
+            var filePath = "avatars/" + i.login + ".jpg"
+            downloadImageByURL(avatarUrl, filePath)
 
-  }
-});
-
-
-
-
-
+        }
+    });
+}
