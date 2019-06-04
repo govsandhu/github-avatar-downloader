b5181c682aa96eeb94283f995440ac921171db0b
Govind Sandhu <gc.sandhu@gmail.com>
2019-06-04 00:18:40 +0000

    Authorization created

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,17 @@
 var request = require('request');
+var tokeKey = require('./secrets')
+
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-   request(url, function(err, res, body) {
+   var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        Authorization: 'token' + tokeKey
+      }
+    };
+   request(options, function(err, res, body) {
     cb(err, body);
    });
 }
